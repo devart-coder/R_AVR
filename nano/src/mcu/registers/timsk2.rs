39dca6d1a9353ee1ce3b58cf57a8aed104e4a3f5
devart-coder <devart.mail@mail.ru>
2026-05-21 01:34:49 +0300

    Timsk2 was realized.

--- nano/src/mcu/registers/timsk2.rs
+++ nano/src/mcu/registers/timsk2.rs
@@ -1,11 +1,24 @@
+use crate::utils::bit_operations::BitOperations;
+
 pub enum Timsk2Bits{
     Toie2  = 0,
     Ocie2a = 1,
     Ocie2b = 2,
 }
 impl Timsk2Bits{
-    const TOIR2:u8  = Timsk2Bits::Toie2  as u8;
+    const TOIE2:u8  = Timsk2Bits::Toie2  as u8;
     const OCIE2A:u8 = Timsk2Bits::Ocie2a as u8;
     const OCIE2B:u8 = Timsk2Bits::Ocie2b as u8;
 }
-
+pub struct Timsk2Builder (pub u8);
+impl Timsk2Builder{
+    pub fn toie2(self)->BitOperations<{Timsk2Bits::TOIE2},Self>{
+        BitOperations::<{Timsk2Bits::TOIE2},Self>(self)
+    }
+    pub fn ocie2a(self)->BitOperations<{Timsk2Bits::OCIE2A},Self>{
+        BitOperations::<{Timsk2Bits::OCIE2A},Self>(self)
+    }
+    pub fn ocie2b(self)->BitOperations<{Timsk2Bits::OCIE2B},Self>{
+        BitOperations::<{Timsk2Bits::OCIE2B},Self>(self)
+    }
+}
--- nano/src/utils/bitsable.rs
+++ nano/src/utils/bitsable.rs
@@ -4,7 +4,7 @@ use crate::mcu::registers::{tccr1a::Tccr1aBuilder,tccr1b::Tccr1bBuilder};
 use crate::mcu::registers::{tccr2a::Tccr2aBuilder,tccr2b::Tccr2bBuilder};
 use crate::mcu::registers::timsk0::Timsk0Builder;
 use crate::mcu::registers::timsk1::Timsk1Builder;
-// use crate::mcu::registers::timsk2::Timsk2Builder;
+use crate::mcu::registers::timsk2::Timsk2Builder;
 pub trait Bitsable{
     fn set_mask(&mut self, mask:u8);
     fn reset_mask(&mut self, mask:u8);
