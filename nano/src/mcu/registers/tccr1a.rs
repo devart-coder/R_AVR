0853d1908ce44d4b43877f750dd3aacac5c8030f
devart-coder <devart.mail@mail.ru>
2026-05-21 01:08:29 +0300

    Tccr1b was realized

--- nano/src/mcu/registers/tccr1a.rs
+++ nano/src/mcu/registers/tccr1a.rs
@@ -1,9 +1,40 @@
+use crate::utils::bit_operations::BitOperations;
+
 #[repr(u8)]
 pub enum Tccr1aBits{
-    WGM10  = 0,
-    WGM11  = 1,
-    COM1B0 = 4,
-    COM1B1 = 5,
-    COM1A0 = 6,
-    COM1A1 = 7,
+    Wgm10  = 0,
+    Wgm11  = 1,
+    Com1b0 = 4,
+    Com1b1 = 5,
+    Com1a0 = 6,
+    Com1a1 = 7,
+}
+impl Tccr1aBits {
+    const WGM10 :u8 = Self::Wgm10 as u8;
+    const WGM11 :u8 = Self::Wgm11 as u8;
+    const COM1B0:u8 = Self::Com1b0 as u8;
+    const COM1B1:u8 = Self::Com1b1 as u8;
+    const COM1A0:u8 = Self::Com1a0 as u8;
+    const COM1A1:u8 = Self::Com1a1 as u8;
+}
+pub struct Tccr1aBuilder (pub u8);
+impl Tccr1aBuilder{
+    pub fn wgm10(self)->BitOperations<{Tccr1aBits::WGM10},Self>{
+        BitOperations::<{Tccr1aBits::WGM10},Self>(self)
+    }
+    pub fn wgm11(self)->BitOperations<{Tccr1aBits::WGM11},Self>{
+        BitOperations::<{Tccr1aBits::WGM11},Self>(self)
+    }
+    pub fn com1b0(self)->BitOperations<{Tccr1aBits::COM1B0},Self>{
+        BitOperations::<{Tccr1aBits::COM1B0},Self>(self)
+    }
+    pub fn com1b1(self)->BitOperations<{Tccr1aBits::COM1B1},Self>{
+        BitOperations::<{Tccr1aBits::COM1B1},Self>(self)
+    }
+    pub fn com1a0(self)->BitOperations<{Tccr1aBits::COM1A0},Self>{
+        BitOperations::<{Tccr1aBits::COM1A0},Self>(self)
+    }
+    pub fn com1a1(self)->BitOperations<{Tccr1aBits::COM1A1},Self>{
+        BitOperations::<{Tccr1aBits::COM1A1},Self>(self)
+    }
 }
--- nano/src/utils/bitsable.rs
+++ nano/src/utils/bitsable.rs
@@ -1,7 +1,7 @@
 use crate::mcu::registers::{eicra::EicraBuilder, eifr::EifrBuilder, eimsk::EimskBuilder};
 use crate::mcu::registers::{tccr0a::Tccr0aBuilder,tccr0b::Tccr0bBuilder};
-use crate::mcu::registers::{tccr1a::*,tccr1b::*};
-use crate::mcu::registers::{tccr2a::*,tccr2b::*};
+use crate::mcu::registers::{tccr1a::Tccr1aBuilder,tccr1b::Tccr1bBuilder};
+use crate::mcu::registers::{tccr2a::Tccr2aBuilder,tccr2b::Tccr2aBuilder};
 pub trait Bitsable{
     fn set_mask(&mut self, mask:u8);
     fn reset_mask(&mut self, mask:u8);
@@ -22,4 +22,4 @@ impl_bits_able!(EicraBuilder,EimskBuilder,EifrBuilder);
 impl_bits_able!(Tccr0aBuilder,Tccr0bBuilder);
 impl_bits_able!(Tccr1aBuilder,Tccr1bBuilder);
 impl_bits_able!(Tccr2aBuilder,Tccr2bBuilder);
-impl_bits_able!(Timsk0Builder,Timsk1Builder, Timsk2Builder);
+impl_bits_able!(Timsk0Builder,Timsk1Builder,Timsk2Builder);
