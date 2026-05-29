55576a9b742b2b0a13f276f72b02ed2cc709e50d
devart-coder <devart.mail@mail.ru>
2026-05-30 01:33:58 +0300

    [New] * Twi:   ** add 'twsr' register.   ** 'twcr' type was switched from 'Register' to struct 'Twcr' * Twcr:   ** 'read','write' public methods was created .   ** 'modify' method was created for TwcrBuilder only.   ** 'address()' was repaced to const value.

--- nano/src/drivers/twi/twi.rs
+++ nano/src/drivers/twi/twi.rs
@@ -1,106 +1,86 @@
-use crate::{ mcu::registers::{twcr::{Twcr, TwcrBuilder}, twdr::Twdr}, utils::register::Register};
+use crate::{
+    drivers::{Uart, uart::uart},
+    mcu::registers::{
+        twcr::{Twcr, TwcrBuilder},
+        twdr::Twdr,
+        twsr::Twsr,
+    },
+    utils::register::Register,
+};
 
-pub struct Twi{
-    twcr:Register,
-    twdr:Register,
+pub struct Twi {
+    twcr: Twcr,
+    twdr: Register,
+    twsr: Register,
 }
-//UserInterface
-//twi
-//.master()
-//.start()
-//.write(u8)
-//.read() || .read_ack()
-//.stop();
-
-//Private
-//read_status(u8);
-//read_int_flag();
-impl Twi{
-    const READ:u8 = 1;
-    const WRITE:u8 = 0;
-    pub fn new()->Self{
-        Self{
-            twcr : Register::new(Twcr::address()),
-            twdr : Register::new(Twdr::address()),
+impl Twi {
+    const READ: u8 = 1;
+    const WRITE: u8 = 0;
+    pub fn new() -> Self {
+        Self {
+            twcr: Twcr::new(),
+            twdr: Register::new(Twdr::address()),
+            twsr: Register::new(Twsr::address()),
         }
     }
-    fn _check_twint_flag_set(self){
-        let value = 
-        TwcrBuilder(0)
-        .twint().set_bit().build();
-        while(self.twcr.read() & value) == 0 {};
-    }
-    // fn _check_status_code(self,status:StatusCode)->bool{
-    //     if (self.twcr.read() & 0xF8) != status {
-    //         false
-    //     } else{
-    //         true
-    //     }
-    // }
-    
-    pub fn start(self)->Self{
-        let value = 
-            TwcrBuilder(0)
-            .twen().set_bit()
-            .twsta().set_bit()
-            .twint().set_bit()
-            .build();
-        self.twcr.modify(|v| v|value);
+    fn _check_twint_flag_set(self) -> Self {
+        let value = TwcrBuilder(0).twint().set_bit().build();
+        while (self.twcr.read() & value) == 0 {}
         self
     }
-    pub fn stop(self)->Self{
-        let value = 
-            TwcrBuilder(0)
-            .twen().set_bit()
-            .twsto().set_bit()
-            .twint().set_bit()
-            .build();
-        self.twcr.modify(|v| v|value);
+    fn _check_status_code(&self, status: u8) -> Result<u8, u8> {
+        match self.twsr.read() & 0xF8 == status {
+            true => Ok(status),
+            false => Err(0),
+        }
+    }
+
+    pub fn start(self) -> Self {
+        self.twcr
+            .modify(|v| v.twen().set_bit().twsta().set_bit().twint().set_bit());
+        let s = self._check_twint_flag_set();
+        match s._check_status_code(StatusCode::STARTED) {
+            Err(r) => {
+                let uart = uart::Uart::get();
+                uart.output.send_slice("[Error] ");
+                uart.output.send_slice(str::from_utf8(&[r]).unwrap());
+            }
+            Ok(_) => (),
+        };
+        s
+    }
+    pub fn stop(self) -> Self {
+        self.twcr
+            .modify(|v| v.twen().set_bit().twsto().set_bit().twint().set_bit());
         self
     }
-    pub fn write_sla_w(self,val:u8)->Self{
+    pub fn send_address_with_write(self, val: u8) -> Self {
         self.write_data(val << 1 | Self::WRITE)
+            ._check_twint_flag_set()
     }
-    pub fn write_sla_r(self,val:u8)->Self{
+    pub fn send_address_with_read(self, val: u8) -> Self {
         self.write_data(val << 1 | Self::READ)
+            ._check_twint_flag_set()
     }
-    pub fn write_data(self,val:u8)->Self{
+    pub fn write_data(self, val: u8) -> Self {
         self.twdr.write(val);
-        let value = 
-            TwcrBuilder(0)
-            .twint().set_bit()
-            .twen().set_bit()
-            .build();
-        self.twcr.modify(|v| v|value);
+        self.twcr.modify(|v| v.twint().set_bit().twen().set_bit());
         self
     }
 }
-//StatusCode(Master::Rx::Started)
-//StatusCode(Master::Tx::Started)
-//StatusCode(Slave::Tx::Started)
-//StatusCode(Slave::Rx::Started)
-enum StatusCode{
-    Master(Master),
-}
-enum Master{
-    Rx(MasterRx),
-    Tx(MasterTx),
-}
+pub enum StatusCode {}
+impl StatusCode {
+    const STARTED: u8 = 0x08;
+    const START_REPEATED: u8 = 0x10;
 
-enum MasterTx{
-    Started         = 0x08,
-    StartRepeated   = 0x10,
-    SlawAck         = 0x18,
-    SlawNack        = 0x20,
-    DataAck         = 0x28,
-    DataNack        = 0x30,
-    Lost            = 0x38,
-}
+    const DATA_WACK: u8 = 0x50;
+    const DATA_WNACK: u8 = 0x58;
 
-enum MasterRx{
-    Started         = 0x08,
-    StartRepeated   = 0x10,
-    SlarLost        = 0x38,
-    DataAck         = 0x50,
-    DataNack        = 0x58,
+    const DATA_RACK: u8 = 0x28;
+    const DATA_RNACK: u8 = 0x30;
+
+    const SLAW_ACK: u8 = 0x18;
+    const SLAW_NACK: u8 = 0x20;
+
+    const LOST: u8 = 0x38;
 }
--- nano/src/mcu/registers/twcr.rs
+++ nano/src/mcu/registers/twcr.rs
@@ -1,52 +1,73 @@
-use crate::utils::bit_operations::BitOperations;
-pub struct Twcr;
-impl Twcr{
-    pub fn address()->u8{ 0xBC }    
+use crate::utils::{bit_operations::BitOperations, register::Register};
+pub struct Twcr {
+    register: Register,
+}
+impl Twcr {
+    const ADDRESS: u8 = 0xBC;
+    pub fn new() -> Self {
+        Self {
+            register: Register::new(Self::ADDRESS),
+        }
+    }
+    pub fn modify<F>(&self, builder: F)
+    where
+        F: FnOnce(TwcrBuilder) -> TwcrBuilder,
+    {
+        let result = builder(TwcrBuilder(0)).build();
+        self.register.modify(|r| r | result);
+    }
+    pub fn write(&self, value: u8) {
+        self.register.write(value);
+    }
+    pub fn read(&self) -> u8 {
+        self.register.read()
+    }
 }
 
-pub enum TwcrBits{ //TwiControlRegister
-    Twie  = 0,//TWIInterruptEnable
-    Twen  = 2,//TWIEnable
-    Twwc  = 3,//TWIWriteCollisionFlag
-    Twsto = 4,//TWIStop
-    Twsta = 5,//TWIStart
-    Twea  = 6,//TWIEnableAcknowledge
-    Twin  = 7,//TWIInterrruptFlag
+pub enum TwcrBits {
+    //TwiControlRegister
+    Twie = 0,  //TWIInterruptEnable
+    Twen = 2,  //TWIEnable
+    Twwc = 3,  //TWIWriteCollisionFlag
+    Twsto = 4, //TWIStop
+    Twsta = 5, //TWIStart
+    Twea = 6,  //TWIEnableAcknowledge
+    Twin = 7,  //TWIInterrruptFlag
 }
-impl TwcrBits{
-    const TWIE:u8 = Self::Twie as u8;
-    const TWEN:u8 = Self::Twen as u8;
-    const TWWC:u8 = Self::Twwc as u8;
-    const TWSTO:u8 = Self::Twsto as u8;
-    const TWSTA:u8 = Self::Twsta as u8;
-    const TWEA:u8 = Self::Twea as u8;
-    const TWIN:u8 = Self::Twin as u8;
+impl TwcrBits {
+    const TWIE: u8 = Self::Twie as u8;
+    const TWEN: u8 = Self::Twen as u8;
+    const TWWC: u8 = Self::Twwc as u8;
+    const TWSTO: u8 = Self::Twsto as u8;
+    const TWSTA: u8 = Self::Twsta as u8;
+    const TWEA: u8 = Self::Twea as u8;
+    const TWIN: u8 = Self::Twin as u8;
 }
 
-pub struct TwcrBuilder (pub u8);
-impl TwcrBuilder{
-    pub fn twie(self)->BitOperations<{TwcrBits::TWIE},Self>{
-        BitOperations::<{TwcrBits::TWIE},Self>(self)
+pub struct TwcrBuilder(pub u8);
+impl TwcrBuilder {
+    pub fn twie(self) -> BitOperations<{ TwcrBits::TWIE }, Self> {
+        BitOperations::<{ TwcrBits::TWIE }, Self>(self)
     }
-    pub fn twen(self)->BitOperations<{TwcrBits::TWEN},Self>{
-        BitOperations::<{TwcrBits::TWEN},Self>(self)
+    pub fn twen(self) -> BitOperations<{ TwcrBits::TWEN }, Self> {
+        BitOperations::<{ TwcrBits::TWEN }, Self>(self)
     }
-    pub fn twwc(self)->BitOperations<{TwcrBits::TWWC},Self>{
-        BitOperations::<{TwcrBits::TWWC},Self>(self)
+    pub fn twwc(self) -> BitOperations<{ TwcrBits::TWWC }, Self> {
+        BitOperations::<{ TwcrBits::TWWC }, Self>(self)
     }
-    pub fn twsto(self)->BitOperations<{TwcrBits::TWSTO},Self>{
-        BitOperations::<{TwcrBits::TWSTO},Self>(self)
+    pub fn twsto(self) -> BitOperations<{ TwcrBits::TWSTO }, Self> {
+        BitOperations::<{ TwcrBits::TWSTO }, Self>(self)
     }
-    pub fn twsta(self)->BitOperations<{TwcrBits::TWSTA},Self>{
-        BitOperations::<{TwcrBits::TWSTA},Self>(self)
+    pub fn twsta(self) -> BitOperations<{ TwcrBits::TWSTA }, Self> {
+        BitOperations::<{ TwcrBits::TWSTA }, Self>(self)
     }
-    pub fn twea(self)->BitOperations<{TwcrBits::TWEA},Self>{
-        BitOperations::<{TwcrBits::TWEA},Self>(self)
+    pub fn twea(self) -> BitOperations<{ TwcrBits::TWEA }, Self> {
+        BitOperations::<{ TwcrBits::TWEA }, Self>(self)
     }
-    pub fn twint(self)->BitOperations<{TwcrBits::TWIN},Self>{
-        BitOperations::<{TwcrBits::TWIN},Self>(self)
+    pub fn twint(self) -> BitOperations<{ TwcrBits::TWIN }, Self> {
+        BitOperations::<{ TwcrBits::TWIN }, Self>(self)
     }
-    pub fn build(self)->u8{
+    pub fn build(self) -> u8 {
         self.0
     }
 }
