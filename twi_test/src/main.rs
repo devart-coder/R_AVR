105f1b30afbaabae2db98f94ea2499b9a71e6fe8
devart-coder <devart.mail@mail.ru>
2026-05-31 15:39:30 +0300

    Update main.rs

--- twi_test/src/main.rs
+++ twi_test/src/main.rs
@@ -21,14 +21,14 @@ fn main() {
     uart.settings().default();
 
     let twi = Twi::new();
-    let master = twi.prescaler_mode(PrescalerMode::_1).speed(123).as_master();
+    let master = twi.prescaler_mode(PrescalerMode::_1).speed(72).as_master();
     let master = match master.start() {
         Ok(s) => {
-            uart.output().send_slice("Start [OK]\n");
+            uart.output().send_slice("Start... [OK]\n");
             s
         }
         Err(err) => {
-            uart.output().send_slice("[Error] ");
+            uart.output().send_slice("Start... [Error] ");
             uart.output().send_slice(str::from_utf8(&[err]).unwrap());
             uart.output().send_slice("\n");
             return;
@@ -36,15 +36,39 @@ fn main() {
     };
     let master = match master.send_address_with_write(BMP_180) {
         Ok(s) => {
-            uart.output().send_slice("AddressWrite [OK]");
+            uart.output().send_slice("AddressWrite... [OK]\n");
             s
         }
         Err(err) => {
-            uart.output().send_slice("AddressWrite [Error] ");
+            uart.output().send_slice("AddressWrite... [Error]\n");
             uart.output().send_slice(str::from_utf8(&[err]).unwrap());
             uart.output().send_slice("\n");
             return;
         }
     };
-    master.write_data(0xff).stop();
+    let master = match master.write_data(0xF4) {
+        Ok(this) => {
+            uart.output().send_slice("WriteData... [OK]\n");
+            this
+        }
+        Err(err) => {
+            uart.output().send_slice("WriteData... [Error] ");
+            uart.output().send_slice(str::from_utf8(&[err]).unwrap());
+            uart.output().send_slice("\n");
+            return;
+        }
+    };
+    let master = match master.write_data(0x2E) {
+        Ok(this) => {
+            uart.output().send_slice("WriteData... [OK]\n");
+            this
+        }
+        Err(err) => {
+            uart.output().send_slice("WriteData... [Error] ");
+            uart.output().send_slice(str::from_utf8(&[err]).unwrap());
+            uart.output().send_slice("\n");
+            return;
+        }
+    };
+    master.stop();
 }
