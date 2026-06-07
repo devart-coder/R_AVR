b80ae03858e3f6ce7ecb7512de9812521821e763
devart-coder <devart.mail@mail.ru>
2026-06-07 13:21:58 +0300

    [News] * 'Master' remake:   -> 'restart' was appented.   -> pub fn renamed from 'check_status_code' to 'expect_status'.   -> pub fn 'read_twdr','read_with_ack','read_with_nack','read_array'   were appented.   -> StatusCode was moved to the mod.

--- nano/src/drivers/twi/mod.rs
+++ nano/src/drivers/twi/mod.rs
@@ -1,2 +1,21 @@
-mod master;
+pub mod master;
 pub mod twi;
+pub enum StatusCode {}
+impl StatusCode {
+    pub const STARTED: u8 = 0x08;
+    pub const START_REPEATED: u8 = 0x10;
+
+    pub const DATA_W_ACK: u8 = 0x50;
+    pub const DATA_W_NACK: u8 = 0x58;
+
+    pub const DATA_R_ACK: u8 = 0x28;
+    pub const DATA_R_NACK: u8 = 0x30;
+
+    pub const SLA_W_ACK: u8 = 0x18;
+    pub const SLA_W_NACK: u8 = 0x20;
+
+    pub const SLA_R_ACK: u8 = 0x40;
+    pub const SLA_R_NACK: u8 = 0x48;
+
+    pub const LOST: u8 = 0x38;
+}
