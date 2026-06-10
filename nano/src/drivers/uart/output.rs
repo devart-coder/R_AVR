878a1c742284f6808f2894ffced99117280b2b95
devart-coder <devart.mail@mail.ru>
2026-06-11 02:39:33 +0300

    [News] * Bmp_180   -> Debug mode: information was rebuilde. Now if device has error, then   it send error code by uart protocol.   -> Fixed some bugs inside StatusCode enum. * Uart::output   -> 'send_bytes' method was applied. Also other method were rebuild   with use new method. * Uart::settings   -> 'default' method now used not 'baud_rate' method.   -> all methods of settings now return mut reference of self.

--- nano/src/drivers/uart/output.rs
+++ nano/src/drivers/uart/output.rs
@@ -11,24 +11,31 @@ impl Output {
             udr0: Register::new(UartRegisters::UDR0 as u8),
         }
     }
-    pub fn send_slice(&self, string: &str) {
-        for byte in string.as_bytes() {
+    pub fn send_slice(&self, string: &str) -> &Self {
+        self.send_bytes(string.as_bytes())
+    }
+    pub fn send_bytes(&self, bytes: &[u8]) -> &Self {
+        for byte in bytes {
             while (self.ucsr0a.read() & (1 << Ucsr0aBits::UDRE0 as u8)) == 0 {}
             self.udr0.write(*byte);
         }
+        self
     }
-    pub fn send_number<T>(&self, v: T)
+    pub fn send_slice_ln(&self, string: &str) -> &Self {
+        self.send_slice(string).send_slice("\n")
+    }
+    pub fn send_number<T>(&self, v: T) -> &Self
     where
         T: itoa::Integer,
     {
         let mut num_buff = itoa::Buffer::new();
-        self.send_slice(num_buff.format(v));
+        self.send_slice(num_buff.format(v))
     }
-    pub fn send_bool(&self, value: bool) {
+    pub fn send_bool(&self, value: bool) -> &Self {
         let result = match value {
             true => "true",
             false => "false",
         };
-        self.send_slice(result);
+        self.send_slice(result)
     }
 }
--- nano/src/drivers/uart/settings.rs
+++ nano/src/drivers/uart/settings.rs
@@ -1,16 +1,16 @@
-use crate::utils::register::Register;
+use super::builders::*;
 use super::interrupt::*;
 use super::*;
 use crate::F_CPU;
-use super::builders::*;
-pub struct Settings{
-    ucsr0c:Register,
-    ubrr0h:Register,
-    ubrr0l:Register,
-    interrupt : Interrupt,
+use crate::utils::register::Register;
+pub struct Settings {
+    ucsr0c: Register,
+    ubrr0h: Register,
+    ubrr0l: Register,
+    interrupt: Interrupt,
 }
-impl Settings{
-    pub const fn new()-> Self{
+impl Settings {
+    pub const fn new() -> Self {
         Settings {
             ucsr0c: Register::new(UartRegisters::UCSR0C as u8),
             ubrr0h: Register::new(UartRegisters::UBRR0H as u8),
@@ -18,59 +18,83 @@ impl Settings{
             interrupt: Interrupt::new(),
         }
     }
-    pub fn double_speed(&self, value:bool){
+    pub fn double_speed(&self, value: bool) -> &Self {
         let ucsr0a = Register::new(UartRegisters::UCSR0A as u8);
         if value == true {
-            ucsr0a.modify(|reg| reg|(1<<Ucsr0aBits::U2X0 as u8));
-        }else{
-            ucsr0a.modify(|reg| reg&(1<<Ucsr0aBits::U2X0 as u8));
+            ucsr0a.modify(|reg| reg | (1 << Ucsr0aBits::U2X0 as u8));
+        } else {
+            ucsr0a.modify(|reg| reg & (1 << Ucsr0aBits::U2X0 as u8));
         }
+        self
     }
-    pub fn package_bits(&self, value: PackageBits){
+    pub fn package_bits(&self, value: PackageBits) -> &Self {
         match value {
-            PackageBits::_5 => self.ucsr0c.modify(|v| v|Ucsr0cBuilder(0).dis_ucsz00().dis_ucsz01().build()),
-            PackageBits::_6 => self.ucsr0c.modify(|v| v|Ucsr0cBuilder(0).en_ucsz00().build()),
-            PackageBits::_7 => self.ucsr0c.modify(|v| v|Ucsr0cBuilder(0).en_ucsz01().build()),
-            PackageBits::_8 => self.ucsr0c.modify(|v| v|Ucsr0cBuilder(0).en_ucsz00().en_ucsz01().build()),
+            PackageBits::_5 => self
+                .ucsr0c
+                .modify(|v| v | Ucsr0cBuilder(0).dis_ucsz00().dis_ucsz01().build()),
+            PackageBits::_6 => self
+                .ucsr0c
+                .modify(|v| v | Ucsr0cBuilder(0).en_ucsz00().build()),
+            PackageBits::_7 => self
+                .ucsr0c
+                .modify(|v| v | Ucsr0cBuilder(0).en_ucsz01().build()),
+            PackageBits::_8 => self
+                .ucsr0c
+                .modify(|v| v | Ucsr0cBuilder(0).en_ucsz00().en_ucsz01().build()),
             PackageBits::_9 => {
+                todo!()
                 // self.interrupt.ucsr0b.modify(|value| value | (1<<Ucsr0bBits::UCSZ02 as u8));
             }
         }
+        self
     }
-    pub fn parity_mode(&self, value:ParityMode){
+    pub fn parity_mode(&self, value: ParityMode) -> &Self {
         match value {
-            ParityMode::Disabled => self.ucsr0c.modify(|v| v| Ucsr0cBuilder(0).dis_upm00().dis_upm01().build()), 
-            ParityMode::Even     => self.ucsr0c.modify(|v| v| Ucsr0cBuilder(0).en_upm00().build()),
-            ParityMode::Odd      => self.ucsr0c.modify(|v| v| Ucsr0cBuilder(0).en_upm00().en_upm01().build()),
+            ParityMode::Disabled => self
+                .ucsr0c
+                .modify(|v| v | Ucsr0cBuilder(0).dis_upm00().dis_upm01().build()),
+            ParityMode::Even => self
+                .ucsr0c
+                .modify(|v| v | Ucsr0cBuilder(0).en_upm00().build()),
+            ParityMode::Odd => self
+                .ucsr0c
+                .modify(|v| v | Ucsr0cBuilder(0).en_upm00().en_upm01().build()),
         }
+        self
     }
-    pub fn stop_bits(&self, value:StopBits){
+    pub fn stop_bits(&self, value: StopBits) -> &Self {
         match value {
-            StopBits::_1=> self.ucsr0c.modify(|v| v| Ucsr0cBuilder(0).dis_usbs0().build()),
-            StopBits::_2=> self.ucsr0c.modify(|v| v| Ucsr0cBuilder(0).en_usbs0().build()),
+            StopBits::_1 => self
+                .ucsr0c
+                .modify(|v| v | Ucsr0cBuilder(0).dis_usbs0().build()),
+            StopBits::_2 => self
+                .ucsr0c
+                .modify(|v| v | Ucsr0cBuilder(0).en_usbs0().build()),
         }
+        self
     }
-    pub fn baud_rate(&self, value:BaudRate){
-        let speed:u32 = match value{
-            BaudRate::_115200=>{
+    pub fn baud_rate(&self, value: BaudRate) -> &Self {
+        let speed: u32 = match value {
+            BaudRate::_115200 => {
                 self.double_speed(true);
                 8
             }
-            _=>{
+            _ => {
                 self.double_speed(false);
                 16
             }
         };
-        let speed=(F_CPU/(speed*(value as u32)))-1;
-        self.ubrr0h.write((speed>>8)as u8);
+        let speed = (F_CPU / (speed * (value as u32))) - 1;
+        self.ubrr0h.write((speed >> 8) as u8);
         self.ubrr0l.write(speed as u8);
+        self
     }
-    pub fn default(&mut self){
-        self.baud_rate(BaudRate::_9600);
-        self.package_bits(PackageBits::_8);
-        self.parity_mode(ParityMode::Disabled);
-        self.stop_bits(StopBits::_1);
+    pub fn default(&mut self) -> &Self {
+        self.package_bits(PackageBits::_8)
+            .parity_mode(ParityMode::Disabled)
+            .stop_bits(StopBits::_1);
         self.interrupt.enable_rxi(true);
         self.interrupt.enable_txi(true);
+        self
     }
 }
