02ba4602076b848631770c220f15c53ffbededed
devart-coder <devart.mail@mail.ru>
2026-06-10 03:06:16 +0300

    [News] * Bmp_180   -> Library was branched to 'Debug' and 'Release' modes.

--- bmp_180/src/bmp_180.rs
+++ bmp_180/src/bmp_180.rs
@@ -30,118 +30,169 @@ impl Bmp180 {
             m: [0; 3],
         }
     }
-
     fn to_read_init(&self, address: u8) {
-        let uart = Uart::get();
-        uart.settings().default();
+        #[cfg(not(debug_assertions))]
+        {
+            self.twi
+                .start()
+                .expect_status(StatusCode::STARTED)
+                .unwrap()
+                .send_address_with_write(Self::BMP_180)
+                .expect_status(StatusCode::SLA_W_ACK)
+                .unwrap()
+                .write_data(address)
+                .expect_status(StatusCode::DATA_R_ACK)
+                .unwrap()
+                .start()
+                .expect_status(StatusCode::START_REPEATED)
+                .unwrap()
+                .send_address_with_read(Self::BMP_180)
+                .expect_status(StatusCode::SLA_R_ACK)
+                .unwrap();
+        }
+        #[cfg(debug_assertions)]
+        {
+            let uart = Uart::get();
+            uart.settings().default();
 
-        match self.twi.start().expect_status(StatusCode::STARTED) {
-            Ok(ok) => {
-                uart.output().send_slice("[Ok] start\n");
+            match self.twi.start().expect_status(StatusCode::STARTED) {
+                Ok(ok) => {
+                    uart.output().send_slice("[Ok] start\n");
+                }
+                Err(err) => {
+                    uart.output().send_slice("[Error] StatusCode: ");
+                    uart.output().send_number(err);
+                    uart.output().send_slice("\n");
+                }
+            };
+            match self
+                .twi
+                .send_address_with_write(Self::BMP_180)
+                .expect_status(StatusCode::SLA_W_ACK)
+            {
+                Ok(ok) => {
+                    uart.output().send_slice("[Ok] AddressWrite\n");
+                }
+                Err(err) => {
+                    uart.output().send_slice("[Error] StatusCode: ");
+                    uart.output().send_number(err);
+                    uart.output().send_slice("\n");
+                }
             }
-            Err(err) => {
-                uart.output().send_slice("[Error] StatusCode: ");
-                uart.output().send_number(err);
-                uart.output().send_slice("\n");
+            match self
+                .twi
+                .write_data(address)
+                .expect_status(StatusCode::DATA_R_ACK)
+            {
+                Ok(ok) => {
+                    uart.output().send_slice("[Ok] DataWrite\n");
+                }
+                Err(err) => {
+                    uart.output().send_slice("[Error] StatusCode: ");
+                    uart.output().send_number(err);
+                    uart.output().send_slice("\n");
+                }
             }
-        };
-        match self
-            .twi
-            .send_address_with_write(Self::BMP_180)
-            .expect_status(StatusCode::SLA_W_ACK)
-        {
-            Ok(ok) => {
-                uart.output().send_slice("[Ok] AddressWrite\n");
+            match self.twi.start().expect_status(StatusCode::START_REPEATED) {
+                Ok(ok) => {
+                    uart.output().send_slice("[Ok] Restart\n");
+                }
+                Err(err) => {
+                    uart.output().send_slice("[Error] StatusCode: ");
+                    uart.output().send_number(err);
+                    uart.output().send_slice("\n");
+                }
             }
-            Err(err) => {
-                uart.output().send_slice("[Error] StatusCode: ");
-                uart.output().send_number(err);
-                uart.output().send_slice("\n");
-            }
-        }
-        match self
-            .twi
-            .write_data(address)
-            .expect_status(StatusCode::DATA_R_ACK)
-        {
-            Ok(ok) => {
-                uart.output().send_slice("[Ok] DataWrite\n");
-            }
-            Err(err) => {
-                uart.output().send_slice("[Error] StatusCode: ");
-                uart.output().send_number(err);
-                uart.output().send_slice("\n");
-            }
-        }
-        match self.twi.start().expect_status(StatusCode::START_REPEATED) {
-            Ok(ok) => {
-                uart.output().send_slice("[Ok] Restart\n");
-            }
-            Err(err) => {
-                uart.output().send_slice("[Error] StatusCode: ");
-                uart.output().send_number(err);
-                uart.output().send_slice("\n");
-            }
-        }
-        match self
-            .twi
-            .send_address_with_read(Self::BMP_180)
-            .expect_status(StatusCode::SLA_R_ACK)
-        {
-            Ok(ok) => {
-                uart.output().send_slice("[Ok] AddressToRead\n");
-            }
-            Err(err) => {
-                uart.output().send_slice("[Error] StatusCode: ");
-                uart.output().send_number(err);
-                uart.output().send_slice("\n");
+            match self
+                .twi
+                .send_address_with_read(Self::BMP_180)
+                .expect_status(StatusCode::SLA_R_ACK)
+            {
+                Ok(ok) => {
+                    uart.output().send_slice("[Ok] AddressToRead\n");
+                }
+                Err(err) => {
+                    uart.output().send_slice("[Error] StatusCode: ");
+                    uart.output().send_number(err);
+                    uart.output().send_slice("\n");
+                }
             }
         }
     }
-    fn read_reg_from(&self, address: u8) -> u16 {
-        let uart = Uart::get();
-        uart.settings().default();
-
+    fn read_reg_from(&mut self, address: u8) -> u16 {
         self.to_read_init(address);
         let mut result: [u8; 2] = [0; 2];
-        if let Ok(v) = self.twi.read_ack().expect_status(StatusCode::DATA_W_ACK) {
-            result[0] = v.read_twdr();
+        #[cfg(not(debug_assertions))]
+        {
+            if let Ok(v) = self.twi.read_ack().expect_status(StatusCode::DATA_W_ACK) {
+                result[0] = v.read_twdr();
+            }
+            if let Ok(v) = self.twi.read_ack().expect_status(StatusCode::DATA_W_ACK) {
+                result[1] = v.read_twdr();
+            }
         }
-        if let Ok(v) = self.twi.read_ack().expect_status(StatusCode::DATA_W_ACK) {
-            result[1] = v.read_twdr();
+        #[cfg(debug_assertions)]
+        {
+            let uart = Uart::get();
+            uart.settings().default();
+            match self.twi.read_ack().expect_status(StatusCode::DATA_W_ACK) {
+                Ok(this) => result[0] = this.read_twdr(),
+                Err(err) => {
+                    uart.output().send_slice("[Error] StatusCode: ");
+                    uart.output().send_number(err);
+                    uart.output().send_slice("\n");
+                }
+            }
+            match self.twi.read_ack().expect_status(StatusCode::DATA_W_ACK) {
+                Ok(this) => result[1] = this.read_twdr(),
+                Err(err) => {
+                    uart.output().send_slice("[Error] StatusCode: ");
+                    uart.output().send_number(err);
+                    uart.output().send_slice("\n");
+                }
+            }
         }
         self.twi.stop();
         u16::from_be_bytes(result)
     }
     fn read_u16(&self) -> u16 {
-        let uart = Uart::get();
         let mut result = [0; 2];
-        match self.twi.read_ack().expect_status(StatusCode::DATA_W_ACK) {
-            Ok(this) => {
+        #[cfg(debug_assertions)]
+        {
+            let uart = Uart::get();
+            match self.twi.read_ack().expect_status(StatusCode::DATA_W_ACK) {
+                Ok(this) => {
+                    result[0] = this.read_twdr();
+                }
+                Err(err) => {
+                    uart.output().send_slice("[Error] StatusCode: ");
+                    uart.output().send_number(err);
+                    uart.output().send_slice("\n");
+                }
+            };
+            match self.twi.read_ack().expect_status(StatusCode::DATA_W_ACK) {
+                Ok(this) => {
+                    result[1] = this.read_twdr();
+                }
+                Err(err) => {
+                    uart.output().send_slice("[Error] StatusCode: ");
+                    uart.output().send_number(err);
+                    uart.output().send_slice("\n");
+                }
+            };
+        }
+        #[cfg(not(debug_assertions))]
+        {
+            if let Ok(this) = self.twi.read_ack().expect_status(StatusCode::DATA_W_ACK) {
                 result[0] = this.read_twdr();
-            }
-            Err(err) => {
-                uart.output().send_slice("[Error] StatusCode: ");
-                uart.output().send_number(err);
-                uart.output().send_slice("\n");
-            }
-        };
-        match self.twi.read_ack().expect_status(StatusCode::DATA_W_ACK) {
-            Ok(this) => {
+            };
+            if let Ok(this) = self.twi.read_ack().expect_status(StatusCode::DATA_W_ACK) {
                 result[1] = this.read_twdr();
-            }
-            Err(err) => {
-                uart.output().send_slice("[Error] StatusCode: ");
-                uart.output().send_number(err);
-                uart.output().send_slice("\n");
-            }
-        };
+            };
+        }
         u16::from_be_bytes(result)
     }
-
     pub fn calibration(&mut self) {
-        let uart = Uart::get();
-        uart.settings().default();
         self.to_read_init(0xAA);
         for index in 1..=11 {
             match index {
@@ -154,10 +205,7 @@ impl Bmp180 {
                 7..=8 => {
                     self.b[index - 7] = self.read_u16() as i16;
                 }
-                9..=10 => {
-                    self.m[index - 9] = self.read_u16() as i16;
-                }
-                11 => {
+                9..=11 => {
                     self.m[index - 9] = self.read_u16() as i16;
                 }
                 _ => {}
